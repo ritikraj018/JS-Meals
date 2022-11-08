c1b70b72f72170c72886e816d1f52032ff55862b
Ritik Raj <ritikrajbarh18@gmail.com>
2022-11-09 02:01:11 +0530

    first commit

---
+++ meals.js
@@ -0,0 +1,21 @@
+$.ajax({
+	type:'get',
+	url:'https://www.themealdb.com/api/json/v1/1/filter.php?a=canadian',
+	success : function(response)
+	{
+		for(var i=0; i< response.meals.length; i++)
+		{
+			var newitem = ` <div class="col-md-3 m-2 shadow p-3 mb-5 bg-white rounded text-center">
+				<p>${response.meals[i].strMeal}</p>
+				<image src=${response.meals[i].strMealThumb} class="img-fluid">
+				<p>${response.meals[i].idMeal}</p>
+			</div>`
+
+			$('#myitems').append(newitem)
+		}
+	},
+	error : function(error) 
+	{
+		console.log(error)
+	}
+})
